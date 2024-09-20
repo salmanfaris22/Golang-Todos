fe32cf8308e26d3d127e4df42b4d06af91e02e38
salmanfaris22 <salman567633800@gmail.com>
2024-09-20 17:04:49 +0530

    first commit

---
+++ m/todo1.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Task struct {
+	name      string
+	completed bool
+}
+
+var tasks []Task
+
+func addTask(task string) {
+	newTask := Task{
+		name:      task,
+		completed: false,
+	}
+	tasks = append(tasks, newTask)
+	fmt.Println("Task Added")
+}
+
+func listTask() {
+	if len(tasks) == 0 {
+		fmt.Println("Not Task Avalible Pleas Add Task ...")
+	} else {
+		for i, v := range tasks {
+			status := "not"
+			if v.completed {
+				status = "done"
+			}
+			fmt.Printf("%d : %s[%s]\n", i+1, v.name, status)
+		}
+	}
+
+}
+
+func markCompleter(i int) {
+	if i >= 1 && i <= len(tasks) {
+		tasks[i-1].completed = true
+		fmt.Println("Task Completed")
+	} else {
+		fmt.Printf("invalide Task")
+	}
+}
+
+func editTask(i int, s string) {
+	if i >= 1 && i <= len(tasks) {
+		tasks[i-1].name = s
+		tasks[i-1].completed = false
+		fmt.Println("Task Updated")
+	} else {
+		fmt.Printf("invalide Task")
+	}
+}
+
+func deletTask(i int) {
+	if i >= 1 && i <= len(tasks) {
+		tasks = append(tasks[:i-1], tasks[i:]...)
+		fmt.Println("Task deleted")
+	} else {
+		fmt.Printf("invalide Task")
+	}
+}
+
+func main() {
+	var indexInput int
+	var taskINput, NewTaskInput string
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("_____________________")
+	fmt.Println("Todo Options:")
+	fmt.Println("1 : ADD task")
+	fmt.Println("2 : List task")
+	fmt.Println("3 : Mark as completed task")
+	fmt.Println("4 : Edit task")
+	fmt.Println("5 : Delite task")
+	fmt.Println("Exit")
+	fmt.Println("_____________________")
+
+	for {
+
+		fmt.Println("Enter The Option (HINT := enter 0 Seee The Options)=   ")
+		scanner.Scan()
+		input := scanner.Text()
+		option, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid Number")
+			continue
+		}
+		switch option {
+		case 0:
+
+			fmt.Println("_____________________")
+			fmt.Println("Todo Options:")
+			fmt.Println("1 : ADD task")
+			fmt.Println("2 : List task")
+			fmt.Println("3 : Mark as completed task")
+			fmt.Println("4 : Edit task")
+			fmt.Println("5 : Delite task")
+			fmt.Println("Exit")
+			fmt.Println("_____________________")
+		case 1:
+			fmt.Println("Enter The Task :")
+			scanner.Scan()
+			NewTaskInput = scanner.Text()
+			addTask(NewTaskInput)
+		case 2:
+			fmt.Println("Listing The Tasks :")
+			listTask()
+		case 3:
+			fmt.Println("completed the Tasks")
+			scanner.Scan()
+			indexInput, err = strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("Invalid Number")
+				continue
+			}
+			markCompleter(indexInput)
+		case 4:
+			fmt.Println("Edit Task No :")
+			scanner.Scan()
+			indexInput, err = strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("Invalid Number")
+				continue
+			}
+			fmt.Println("enter new Task  :")
+			scanner.Scan()
+			taskINput = scanner.Text()
+
+			editTask(indexInput, taskINput)
+		case 5:
+			fmt.Println("Delet Task No :")
+			scanner.Scan()
+			indexInput, err = strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("Invalid Number")
+				continue
+			}
+			deletTask(indexInput)
+		case 6:
+			fmt.Println("Exiting......")
+			return
+		}
+
+	}
+
+}
--- main.go
+++ main.go
@@ -2,130 +2,201 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 )
 
-type Task struct {
-	name      string
-	completed bool
+type StoreBlog struct {
+	stor []Blog
 }
 
-var tasks []Task
-
-func addTask(task string) {
-	newTask := Task{
-		name:      task,
-		completed: false,
-	}
-	tasks = append(tasks, newTask)
-	fmt.Println("Task Added")
+type Blog struct {
+	head   string
+	body   string
+	privet bool
 }
 
-func listTask() {
-	for i, v := range tasks {
-		status := "not"
-		if v.completed {
-			status = "done"
+func (s *StoreBlog) addBlog(head, body string) {
+
+	newBlog := Blog{head: head, body: body, privet: true}
+	s.stor = append(s.stor, newBlog)
+	fmt.Println("Bloge Added 🦚")
+	// fmt.Println(s.stor)
+}
+
+func (s *StoreBlog) listBlogs() {
+	if len(s.stor) == 0 {
+		fmt.Println("No Bloge Availible 💔")
+	} else {
+		for i, v := range s.stor {
+			privet := "Public"
+			if v.privet {
+				privet = "Privite"
+			}
+			fmt.Printf("id:%d: 👉🏻title is := %s , \n  🤜🏻%s \n  🥶this Bloge Is %s \n", i+1, v.head, v.body, privet)
 		}
-		fmt.Printf("%d : %s[%s]\n", i+1, v.name, status)
 	}
+
 }
 
-func markCompleter(i int) {
-	if i >= 1 && i <= len(tasks) {
-		tasks[i-1].completed = true
-		fmt.Println("Task Completed")
-	} else {
-		fmt.Printf("invalide Task")
+func (s *StoreBlog) setStaus(i int, b string) error {
+	if i > len(s.stor) || i <= 0 {
+		return errors.New("eror")
 	}
+	if b == "private" {
+		s.stor[i-1].privet = true
+		fmt.Println("Staus Changed 🦚")
+		return nil
+	} else {
+		s.stor[i-1].privet = false
+		fmt.Println("Staus Changed 🦚")
+		return nil
+
+	}
+
 }
 
-func editTask(i int, s string) {
-	if i >= 1 && i <= len(tasks) {
-		tasks[i-1].name = s
-		tasks[i-1].completed = false
-		fmt.Println("Task Updated")
-	} else {
-		fmt.Printf("invalide Task")
+func (s *StoreBlog) editsBlog(i int) error {
+	scanner := bufio.NewScanner(os.Stdin)
+	if i > len(s.stor) || i <= 0 {
+		return errors.New("eror")
 	}
+
+	fmt.Println(" 🦋What You Want Change head Or body Or both")
+	scanner.Scan()
+	get := scanner.Text()
+
+	switch get {
+	case "head":
+		fmt.Println("🪴 Enter Your New Blogs")
+		scanner.Scan()
+		title := scanner.Text()
+		s.stor[i-1].head = title
+		fmt.Println("Bloge Edited 🦚")
+	case "body":
+		fmt.Println("🪴 Enter Your New Blogs")
+		scanner.Scan()
+		body := scanner.Text()
+		s.stor[i-1].body = body
+		fmt.Println("Bloge Edited 🦚")
+	case "both":
+		fmt.Println("🪴 Enter Your New Blogs")
+		scanner.Scan()
+		title := scanner.Text()
+		fmt.Println("🪴 Enter Your New Blogs")
+		scanner.Scan()
+		body := scanner.Text()
+		s.stor[i-1].head = title
+		s.stor[i-1].body = body
+		fmt.Println("Bloge Edited 🦚")
+	default:
+		fmt.Println("Enter valide string")
+
+	}
+
+	return nil
 }
 
-func deletTask(i int) {
-	if i >= 1 && i <= len(tasks) {
-		tasks = append(tasks[:i-1], tasks[i:]...)
-		fmt.Println("Task deleted")
+func (s *StoreBlog) deliteBloge(i int) error {
+	if i > len(s.stor) || i <= 0 {
+		return errors.New("Erre")
 	} else {
-		fmt.Printf("invalide Task")
+		s.stor = append(s.stor[:i-1], s.stor[i:]...)
+		fmt.Println("Bloge Delited")
 	}
+
+	return nil
 }
 
 func main() {
-	var indexInput int
-	var taskINput, NewTaskInput string
-
+	fmt.Println("Welcome To Bloge Apps")
+	store := StoreBlog{}
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Println("Start Todo Options:")
-		fmt.Println("1 : ADD task")
-		fmt.Println("2 : List task")
-		fmt.Println("3 : Mark as completed task")
-		fmt.Println("4 : Edit task")
-		fmt.Println("5 : Delite task")
-		fmt.Println("Exit")
-		fmt.Println("Enter The Option =")
+		fmt.Println("_____________________")
+		fmt.Println("Bloge Option:")
+		fmt.Println("1 : ADD Blog 🫡")
+		fmt.Println("2 : List Blog 🫠")
+		fmt.Println("3 : Set Privet Or Public 🤯")
+		fmt.Println("4 : Edit Blog 🙌🏻")
+		fmt.Println("5 : Delite Blog 👌🏻")
+		fmt.Println("6: Exit")
+		fmt.Println("_____________________")
+		fmt.Println("🫵🏻 Enter Your Option : ")
 		scanner.Scan()
 		input := scanner.Text()
-		option, err := strconv.Atoi(input)
+		newinput, err := strconv.Atoi(input)
 		if err != nil {
-			fmt.Println("Invalid Number")
+			fmt.Println("Invalide Number ")
 			continue
 		}
-		switch option {
+		switch newinput {
 		case 1:
-			fmt.Println("Enter The Task :")
+			fmt.Println(" 🦋Enter Your Blog Title")
 			scanner.Scan()
-			NewTaskInput = scanner.Text()
-			addTask(NewTaskInput)
-		case 2:
-			fmt.Println("Listing The Tasks :")
-			listTask()
-		case 3:
-			fmt.Println("completed the Tasks")
+			title := scanner.Text()
+			fmt.Println("🪴 Enter Your Blogs")
 			scanner.Scan()
-			indexInput, err = strconv.Atoi(scanner.Text())
-			if err != nil {
-				fmt.Println("Invalid Number")
-				continue
-			}
-			markCompleter(indexInput)
-		case 4:
-			fmt.Println("Edit Task No :")
-			scanner.Scan()
-			indexInput, err = strconv.Atoi(scanner.Text())
-			if err != nil {
-				fmt.Println("Invalid Number")
-				continue
-			}
-			fmt.Println("enter new Task  :")
-			scanner.Scan()
-			taskINput = scanner.Text()
+			body := scanner.Text()
+			store.addBlog(title, body)
 
-			editTask(indexInput, taskINput)
-		case 5:
-			fmt.Println("Delet Task No :")
+		case 2:
+			fmt.Println("Listing Bloge🦊 ")
+			store.listBlogs()
+		case 3:
+			fmt.Println("Enter The ID Of Bloge🦊 ")
 			scanner.Scan()
-			indexInput, err = strconv.Atoi(scanner.Text())
+			input := scanner.Text()
+			index, err := strconv.Atoi(input)
 			if err != nil {
-				fmt.Println("Invalid Number")
-				continue
+				fmt.Println("Pleas Enter valid NUmebr")
 			}
-			deletTask(indexInput)
+
+			fmt.Println("public or private")
+			scanner.Scan()
+			status := scanner.Text()
+
+			if status == "public" || status == "private" {
+				err := store.setStaus(index, status)
+				if err != nil {
+					fmt.Println("Enter Your Error")
+				}
+			} else {
+				fmt.Println("Enter Valide ")
+
+			}
+		case 4:
+			fmt.Println("Which On is Your Want Edits enter ID :")
+			scanner.Scan()
+			input, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("Ivalid Id")
+			}
+			er := store.editsBlog(input)
+			if er != nil {
+				fmt.Println("Enter Your Error Id :")
+			}
+		case 5:
+			fmt.Println("Which On is Your Want Delite enter ID :")
+			scanner.Scan()
+			input, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("Ivalid Id")
+			}
+			er := store.deliteBloge(input)
+			if er != nil {
+				fmt.Println("Ivalid Id")
+			}
 		case 6:
-			fmt.Println("Exiting......")
+			fmt.Println("Exited")
 			return
+		default:
+
+			continue
+
 		}
 
 	}
